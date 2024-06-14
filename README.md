51209cb63c99f43c81abf564e87a85239b75396c
codingconcepts <hello@robreid.io>
2024-06-14 15:44:55 +0100

    feat: Unit tests for basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # errhandler
 Wraps Go's http.HandlerFunc, allowing for simpler use of http.NewServeMux().
+
+### Install
+
+```sh
+go get github.com/codingconcepts/errhandler
+```
+
+### Usage
+
+Wrap Go's stdlib `http.HandlerFunc` using `errhandler.Wrap()`:
+
+```go
+mux := http.NewServeMux()
+mux.Handle("GET /products/{id}", errhandler.Wrap(getProduct))
+```
+
+Update your existing handler signature, by adding an `error` return type:
+
+```go
+func addProduct(w http.ResponseWriter, r *http.Request) error {
+	...
+}
+```
+
+Utilize the `ParseJSON` and `SendJSON` functions to reduce the amount of boilerplate code handlers require:
+
+``` go
+var p product
+if err := errhandler.ParseJSON(r, &p); err != nil {
+  return fmt.Errorf("parsing request json: %w", err)
+}
+
+products[p.ID] = p
+
+return errhandler.SendJSON(w, p)
+```
---
+++ errhandler_test.go
@@ -0,0 +1,95 @@
+package errhandler
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// TestWrap_ServeHTTP tests the ServeHTTP method of the Wrap type.
+func TestWrap_ServeHTTP(t *testing.T) {
+	tests := []struct {
+		name           string
+		body           string
+		handler        Wrap
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name: "parse json without error",
+			body: `{"a": 1}`,
+			handler: Wrap(func(w http.ResponseWriter, r *http.Request) error {
+				m := map[string]int{}
+
+				return ParseJSON(r, &m)
+			}),
+			expectedStatus: http.StatusOK,
+			expectedBody:   "",
+		},
+		{
+			name: "parse json with error",
+			body: "a",
+			handler: Wrap(func(w http.ResponseWriter, r *http.Request) error {
+				m := map[string]int{}
+
+				return ParseJSON(r, &m)
+			}),
+			expectedStatus: http.StatusInternalServerError,
+			expectedBody:   "invalid character 'a' looking for beginning of value\n",
+		},
+		{
+			name: "send string without error",
+			handler: Wrap(func(w http.ResponseWriter, r *http.Request) error {
+				return SendString(w, "test")
+			}),
+			expectedStatus: http.StatusOK,
+			expectedBody:   "test",
+		},
+		{
+			name: "send json without error",
+			handler: Wrap(func(w http.ResponseWriter, r *http.Request) error {
+				return SendJSON(w, map[string]int{"a": 1})
+			}),
+			expectedStatus: http.StatusOK,
+			expectedBody:   "{\"a\":1}\n",
+		},
+		{
+			name: "error",
+			handler: Wrap(func(w http.ResponseWriter, r *http.Request) error {
+				return fmt.Errorf("error doing stuff: %w", errors.New("database error"))
+			}),
+			expectedStatus: http.StatusInternalServerError,
+			expectedBody:   "error doing stuff: database error\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var body io.Reader
+			if tt.body != "" {
+				body = strings.NewReader(tt.body)
+			}
+
+			req := httptest.NewRequest(http.MethodGet, "http://example.com/foo", body)
+			w := httptest.NewRecorder()
+
+			tt.handler.ServeHTTP(w, req)
+
+			res := w.Result()
+			defer res.Body.Close()
+
+			if res.StatusCode != tt.expectedStatus {
+				t.Errorf("expected status %d, got %d", tt.expectedStatus, res.StatusCode)
+			}
+
+			actualBody := w.Body.String()
+			if actualBody != tt.expectedBody {
+				t.Errorf("expected body %q, got %q", tt.expectedBody, actualBody)
+			}
+		})
+	}
+}
