21a17eba1f889255dfb66ca743612068df0781b8
codingconcepts <hello@robreid.io>
2024-06-14 15:13:26 +0100

    feat: Basic implementation of required functions

---
+++ errhandler.go
@@ -0,0 +1,43 @@
+package errhandler
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// ErrHandlerFunc wraps http.HandlerFunc, allowing for errors to be handled
+// in a more familiar way inside your handlers.
+type ErrHandlerFunc func(w http.ResponseWriter, r *http.Request) error
+
+// ServeHTTP is invoked when the HTTP handler is called, capturing and returning
+// any errors encountered in the ErrHandlerFunc.
+func (fn ErrHandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := fn(w, r); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// SendJSON returns a JSON response to the caller, or fails with an error.
+func SendJSON(w http.ResponseWriter, data interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		return err
+	}
+	return nil
+}
+
+// SendString returns a string response to the caller, or fails with an error.
+func SendString(w http.ResponseWriter, message string) error {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write([]byte(message)); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ParseJSON can be used to parse a string from the body of a request.
+func ParseJSON(r *http.Request, val any) error {
+	return json.NewDecoder(r.Body).Decode(val)
+}
